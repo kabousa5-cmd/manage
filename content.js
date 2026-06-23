052df981f94a5cd3d0cb5dace5cc1068356bed57
kabousa5-cmd <kabousa5@gmail.com>
2026-06-23 23:18:19 +0100

    Update content.js

--- content.js
+++ content.js
@@ -11,7 +11,8 @@
     'use strict';
 
     // ⚠️ حط هنا الرابط المباشر (RAW) ديال الكود ديالك أونلاين
-    const ONLINE_SCRIPT_URL = "https://pastebin.com/raw/xxxxxx"; 
+   // ⚠️ حط هنا الرابط المباشر (RAW) ديال الكود ديالك أونلاين
+const ONLINE_SCRIPT_URL = "https://github.com/kabousa5-cmd/manage/edit/main/content.js"; 
 
     // أولاً: التأكد واش كاين كود محدث فالمتصفح وتشغيله
     chrome.storage.local.get({ online_code: null }, function(res) {
